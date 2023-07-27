1364a9ddeb474a56d436196605d2bf572451760c
wimdavies <william.johnston.davies@gmail.com>
2023-07-27 17:16:00 +0100

    Ch.4: Refactor into #quantity

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,12 +18,12 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "#{quantity(number - 1)} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{number - 1} #{container(number - 1)} of beer on the wall.\n"      
+      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"      
     end
   end
 
@@ -40,6 +40,14 @@ class Bottles
       'it'
     else
       'one'
+    end
+  end
+
+  def quantity(number=0)
+    if number == 0
+      'no more'
+    else
+      number
     end
   end
 end
