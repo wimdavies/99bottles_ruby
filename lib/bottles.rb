f339055df2b5f63de2d3580482d2413f8694f69d
wimdavies <william.johnston.davies@gmail.com>
2023-07-27 14:12:22 +0100

    Ch.3: Complete horizontal refactoring

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,30 +9,29 @@ class Bottles
 
   def verse(number)
     case number 
-    when 2
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number - 1} bottle of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number - 1} #{container} of beer on the wall.\n"      
+      "#{number - 1} #{container(number - 1)} of beer on the wall.\n"      
     end
   end
 
-  def container(number=:FIXME)
-    'bottles'
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
+    end
   end
 end
