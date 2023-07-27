a7b1b93d2c1b3495131699c3743f54cb1beab210
wimdavies <william.johnston.davies@gmail.com>
2023-07-27 14:37:18 +0100

    Ch.4: #pronoun horizontal refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,14 +15,14 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
+      "Take #{pronoun(number)} down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
+      "Take #{pronoun(number)} down and pass it around, " +
       "#{number - 1} #{container(number - 1)} of beer on the wall.\n"      
     end
   end
@@ -32,6 +32,14 @@ class Bottles
       'bottle'
     else
       'bottles'
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
     end
   end
 end
