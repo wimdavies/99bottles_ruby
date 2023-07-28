59bf300bffc3f5df1f126870476167349460dd02
wimdavies <william.johnston.davies@gmail.com>
2023-07-28 12:39:43 +0100

    4.8: Refactor line 4 to call #quantity with new #successor(number)

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,12 +13,12 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{instruction(number)}, " +
-      "99 bottles of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(number - 1)} of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{instruction(number)}, " +
-      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"      
+      "#{quantity(successor(number))} #{container(number - 1)} of beer on the wall.\n"      
     end
   end
 
@@ -51,6 +51,14 @@ class Bottles
       'no more'
     else
       number.to_s
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
     end
   end
 end
